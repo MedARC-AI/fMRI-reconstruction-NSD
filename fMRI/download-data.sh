05e5d6402c9a6520f363597bfdea450546570967
jimgoo <jimmiegoode@gmail.com>
2022-10-28 15:27:57 +0000

    Add parallel download script

---
+++ fMRI/download-data.sh
@@ -0,0 +1,3 @@
+wget -c https://huggingface.co/datasets/pscotti/naturalscenesdataset/resolve/main/subj01_nsdgeneral.hdf5 &
+wget -c https://huggingface.co/datasets/pscotti/naturalscenesdataset/resolve/main/stylegan_projections.hdf5 &
+wget -c https://huggingface.co/datasets/pscotti/naturalscenesdataset/resolve/main/COCO_73k_subj_indices.hdf5 &
