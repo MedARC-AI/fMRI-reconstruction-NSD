1b4268adbc1270e6bf352c8406236134343d7b35
Paul Scotti <paulpauzle@gmail.com>
2022-11-27 19:21:05 -0500

    tidying up; forwards/backwards retrieval

---
+++ fMRI/ddp_config.py
@@ -0,0 +1,50 @@
+# Distributed Data Parallel initialization
+# Setup multi-gpu PyTorch distributed parallel processing (ddp) if multiple gpus are found
+def ddp_test():
+    try:
+        # set up multi-gpu distributed parallel processing
+        import torch.distributed as dist
+        from torch.nn.parallel import DistributedDataParallel as DDP
+        from socket import gethostname
+
+        rank          = int(os.environ["SLURM_PROCID"])
+        world_size    = int(os.environ["WORLD_SIZE"])
+        gpus_per_node = int(os.environ["SLURM_GPUS_ON_NODE"])
+        assert gpus_per_node == torch.cuda.device_count()
+        print(f"Hello from rank {rank} of {world_size} on {gethostname()} where there are" \
+              f" {gpus_per_node} allocated GPUs per node.", flush=True)
+
+        dist.init_process_group("nccl", rank=rank, world_size=world_size)
+        if rank == 0: print(f"Group initialized? {dist.is_initialized()}", flush=True)
+
+        local_rank = rank - gpus_per_node * (rank // gpus_per_node)
+        print("local rank",local_rank)
+        torch.cuda.set_device(local_rank)
+        using_ddp = True
+    except:
+        local_rank = 0
+        using_ddp = False
+    if using_ddp:
+        print("DistributedDataParallel enabled!")
+    else:
+        print("NOT using distributed parallel processing!")
+    return using_ddp
+
+# Slurm reference for DDP:
+## !/bin/bash
+# SBATCH --job-name=clipvox      #create a short name for your job
+# SBATCH --nodes=1               #node count
+# SBATCH --ntasks-per-node=2    #with DDP, must equal num of gpus
+# SBATCH --cpus-per-task=8     #rule-of-thumb is 4 times number of gpus
+# SBATCH --gres=gpu:2  
+# SBATCH --mem-per-gpu=40G     
+# SBATCH --time=00:10:00       #total run time limit (HH:MM:SS)
+# SBATCH --mail-type=begin,end,fail         
+# SBATCH --mail-user=your_email@gmail.com
+# export MASTER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4))
+# export WORLD_SIZE=$(($SLURM_NNODES * $SLURM_NTASKS_PER_NODE))
+# echo "WORLD_SIZE="$WORLD_SIZE
+# master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+# export MASTER_ADDR=$master_addr
+# echo "MASTER_ADDR="$MASTER_ADDR
+# srun python Voxel_to_CLIPvoxel.py
