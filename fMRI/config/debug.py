50cbcd7ae28289fc582031e6f85cbeb3e0c181de
jimgoo <jimmiegoode@gmail.com>
2023-01-20 15:07:17 -0600

    Update y type aug and docker

---
+++ fMRI/config/debug.py
@@ -0,0 +1,10 @@
+batch_size = 64
+# clip_aug_prob = 0.05
+clip_aug_prob = 0.0
+clip_aug_mode = 'y'
+first_batch = True
+wandb_log = False
+n_samples_save = 2
+ckpt_saving = False
+outdir = "~/data/neuro/models/prior/test"
+num_epochs = 200
