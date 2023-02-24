047fb72122f7b8b82464b65d94c0fdc9b28f97b2
jimgoo <jimmiegoode@gmail.com>
2023-02-24 18:40:33 +0000

    Add FID scoring

--- src/config/1D_combo.py
+++ src/config/1D_combo.py
@@ -1,4 +1,2 @@
 voxel_dims = 1
 batch_size = 64
-wandb_log = True
-outdir = "/home/jimgoo/data/neuro/models/1D-combo"
--- src/config/3D_combo.py
+++ src/config/3D_combo.py
@@ -13,13 +13,8 @@ voxel2clip_kwargs = dict(
     average_output=False,
 )
 
-wandb_log = True
-# wandb_run_name = "3D-combo"
-outdir = "/home/jimgoo/data/neuro/models/3D-combo"
-
 ## debug
 # first_batch = True
 # save_at_end = True
 # ckpt_saving = False
 # num_epochs = 500
-# wandb_project = 'laion-fmri-debug'
